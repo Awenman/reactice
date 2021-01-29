db289d3ab323e3c67d301693b9c8b6962ee8a3ed
Andrew Wenman <andrewwenman@Andrews-MacBook-Pro.local>
2021-01-29 16:32:34 -0600

    next commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App">      <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>and so it begins </code> and save to reload.
+        
+        <p>Here we go again
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
